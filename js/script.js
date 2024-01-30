1daacb3a04ca7eb252b4829d8b12013af67c43d1
AnasKhalif <anaskhalif995@gmail.com>
2024-01-30 17:26:31 +0700

    home section

--- js/script.js
+++ js/script.js
@@ -20,3 +20,18 @@ document.querySelector('#search-icon').onclick = () => {
 document.querySelector('#close').onclick = () => {
     document.querySelector('#search-form').classList.remove('active');
 }
+
+var swiper = new Swiper(".home-slide", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop:true,
+
+  });
